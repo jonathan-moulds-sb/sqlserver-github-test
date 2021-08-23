d9f4d87dd9b2233c01b8f3414eebfbd9d58b7a2a
jonathan-moulds-sb <47507029+jonathan-moulds-sb@users.noreply.github.com>
2021-08-24 09:22:33 +1200

    latest update

---
+++ Scripts/CREATE TABLE dbo.Products.sql
@@ -0,0 +1,6 @@
+CREATE TABLE dbo.Products1  
+   (ProductID int PRIMARY KEY NOT NULL,  
+   ProductName varchar(25) NOT NULL,  
+   Price money NULL,  
+   ProductDescription varchar(max) NULL)  
+GO 
---
+++ Scripts/INSERT dbo.Products.1.sql
@@ -0,0 +1,4 @@
+-- Standard syntax  
+INSERT dbo.Products1 (ProductID, ProductName, Price, ProductDescription)  
+    VALUES (1, 'Clamp', 12.48, 'Workbench clamp')  
+GO
